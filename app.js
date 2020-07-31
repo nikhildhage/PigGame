e72be3917b17227bb5ddac787fa313a9258af301
Nikhil Dhage <nikhil.dhage@gmail.com>
2020-07-31 17:57:54 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+ Game Rules
+  
+ -The game has two players, playing in rounds.
+ - In each round , aplayer rolls the dice as many times as he wishes. Each result gets added to his Round
+ - BUt, if he rolls a 1, all his ROUND score gets lost. After that , its the next players turn.
+ - The player can choose to "Hold", which means his ROUND score gets added to his GLOBAL score. After that , its the next player's turn.
+ -   The first player to reach 100 points on GLOBAL score wins the game.
+  
+ */
