afcac3230a431eb89d0d1a538955657990165fd9
Nikhil Dhage <nikhil.dhage@gmail.com>
2020-08-02 02:53:19 -0400

    added button support

--- app.js
+++ app.js
@@ -8,3 +8,38 @@
  -   The first player to reach 100 points on GLOBAL score wins the game.
   
  */
+
+var rollBtn = document.querySelector(".btn-roll");
+
+// variables
+var scores, roundScore, activePlayer, dice;
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+function setActivePlayer() {
+  this.activePlayer = player;
+}
+
+function getActivePLayer() {
+  return this.activePlayeractivePlayer;
+}
+
+function generateDice() {
+  diceValue = Math.floor(Math.random() * 6) + 1;
+  console.log("Dice value: " + diceValue);
+}
+
+function showRoundScore() {
+  document.querySelector("#current-" + activePlayer).innerHTML =
+    "<em>" + diceValue + "</em>";
+}
+
+rollBtn.addEventListener("click", function () {
+  var diceImg = document.querySelector(".dice");
+  diceImg.style.display = "none";
+  generateDice();
+  showRoundScore();
+  diceImg.style.display = "block";
+  diceImg.src = "images/dice-" + diceValue + ".png";
+});
